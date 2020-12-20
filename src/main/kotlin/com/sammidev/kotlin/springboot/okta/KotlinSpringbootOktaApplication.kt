9508367e54fbb1cdde53123eda4460813b02ae41
SemmiDev <sammidev4@gmail.com>
2020-12-20 09:34:56 +0700

    basic auth

---
+++ src/main/kotlin/com/sammidev/kotlin/springboot/okta/KotlinSpringbootOktaApplication.kt
@@ -0,0 +1,48 @@
+package com.sammidev.kotlin.springboot.okta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class KotlinSpringbootOktaApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringbootOktaApplication>(*args)
+}
+
+@Configuration
+class SecurityConfig() : WebSecurityConfigurerAdapter() {
+	override fun configure(http: HttpSecurity?) {
+		http
+				?.authorizeRequests()
+				?.anyRequest()?.authenticated()
+				?.and()
+				?.httpBasic()
+	}
+
+	override fun configure(auth: AuthenticationManagerBuilder?) {
+		auth?.inMemoryAuthentication()
+				?.withUser("sammidev")
+				?.password("{noop}password")
+				?.roles("USER")
+	}
+}
+
+@RestController
+class Greeting {
+
+	@GetMapping("/")
+	@ResponseBody
+	fun name(@RequestParam("name") name: String): String {
+		return "hello, ${name}"
+	}
+
+}
---
+++ src/test/kotlin/com/sammidev/kotlin/springboot/okta/KotlinSpringbootOktaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sammidev.kotlin.springboot.okta
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringbootOktaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
